29a62ef8ad7a8316aa4b5a7da4ddb828115c177b
kairuflayark <cal12011@byui.edu>
2021-12-01 16:28:35 -0700

    Updated Drinking with totalDrinks Function

--- app/src/main/java/com/longhurst/hydrateme/data/Drinking.kt
+++ app/src/main/java/com/longhurst/hydrateme/data/Drinking.kt
@@ -1,16 +1,29 @@
 package com.longhurst.hydrateme.data
 
+import android.util.Log
+import kotlin.math.ceil
+
 //This file should contain all sorts of different functions for handling how much someone is supposed to drink
-fun amountByWeight(weight: Float): Int{
-    val amount = Math.ceil(weight * 0.5)
-    return amount.toInt()
+fun amountByWeight(weight: Float): Float{
+    val amount = ceil(weight * 0.5)
+    //Log.e("TESTTESTWEIGHT", "$weight $amount")
+    return amount.toFloat()
 }
 
-fun amountofDrinks(waterAmount: Int):Int{
-    val amount:Double = Math.ceil(waterAmount.toDouble() / 8)
-    return amount.toInt()
+fun amountofDrinks(waterAmount: Float):Float{
+    val amount:Float = ceil(waterAmount / 8)
+    //Log.e("TESTTESTDRINKS", "$waterAmount $amount")
+    return amount
 }
 
-fun drinksFromExercise(hours: Int): Int{
+fun drinksFromExercise(hours: Float): Float{
+    //val amount = hours * 4
+    //Log.e("TESTTESTEXERCEISE", "$hours $amount")
     return hours * 3
+}
+
+fun totalDrinks(weight: Float, hours: Float){
+
+    val ounces = amountByWeight(weight)
+    val drinks = amountofDrinks(ounces) + drinksFromExercise(hours)
 }
